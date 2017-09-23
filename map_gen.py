3d08cfa46f30699f21002a74e8c88b39e1936a5d
OdedGinat2 <odedginat@gmail.com>
2017-09-23 23:50:34 +0300

    v1 - ?
    ?

--- map_gen.py
+++ map_gen.py
@@ -10,17 +10,21 @@ from dotmap_types import Brush
 def main():
 
     file = open("brush.map", "w")
+
+    #You need this for the map file to work
     str = "// entity 0\r\n"
     str += "{\r\n"
     str += "\"classname\" \"worldspawn\"\r\n"
 
+    #give the Brush() 8 point, and it shall give you a string
     str += Brush(Vec3(0, 0, 0), Vec3(10, 10, 10), Vec3(10, 0, 0), Vec3(0, 10, 0), Vec3(0, 0, 10), Vec3(10, 10, 0), Vec3(10, 0, 10), Vec3(0, 10, 10)).str
 
+    #this is also needed
     str += "}\r\n"
 
+    #DA
     file.write(str)
 
-    pass  # Replace Pass with Your Code
 
 
 if __name__ == '__main__':
